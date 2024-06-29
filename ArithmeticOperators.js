2e46cc9f0805e5e7c66c281b56ebb82f9486a132
donMaleesha <lochanamt@gmail.com>
2024-06-29 17:56:40 +0530

    Arithmetic operators

---
+++ ArithmeticOperators.js
@@ -0,0 +1,24 @@
+console.log(2 + 2); //4
+
+console.log(1 + 2 + 3); //6
+
+console.log(1 + 2 - 3); //0
+
+console.log(1 + 2 * 3); //7
+
+console.log((1 + 2) * 3); //9
+
+console.log(1 + 2 / 3); //1.6666666666666667
+
+console.log((1 + 2) / 3); //1
+
+console.log(1 + 2 % 3); //3
+
+console.log(1 % 2); //1
+
+console.log(2 % 2); //0
+
+console.log(3 % 2); //1
+
+console.log(4 % 2); //0
+
