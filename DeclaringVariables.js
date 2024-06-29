b8996d71abd00891c4be62ecd40681ff70c53ff1
donMaleesha <lochanamt@gmail.com>
2024-06-29 17:38:31 +0530

    In this exercise, you will practice declaring variables

---
+++ DeclaringVariables.js
@@ -0,0 +1,21 @@
+var petDog = "Rex";
+
+var petCat = "Fluffy";
+
+console.log(petDog);
+console.log(petCat);
+
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+
+var catSound = "purr";
+
+var dogSound = "bark";
+
+console.log(petDog + " says " + dogSound);
+
+console.log(petCat + " says " + catSound);
+
+catSound = "meow";
+
+console.log(petCat + " now says " + catSound);
