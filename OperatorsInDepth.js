7d3e0c3dfd767f7a407afb4f130325d8eb7d00a3
donMaleesha <lochanamt@gmail.com>
2024-07-01 19:25:47 +0530

    Operators cont.

---
+++ OperatorsInDepth.js
@@ -0,0 +1,20 @@
+console.log(10 > 9 && 10 < 17); //true
+
+console.log(true && true); //true
+
+console.log(true && false); //false
+
+console.log(false && true); //false
+
+console.log(false && false); //false
+
+var currentTime = 7;
+console.log(currentTime < 9 || currentTime > 17); //true
+
+console.log(true || true); //true
+
+var petHungry = true;
+
+console.log("Feeding the pet: ");
+console.log("Pet is hungry: " + !petHungry);
+console.log(petHungry);
