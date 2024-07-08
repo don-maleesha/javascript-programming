e490efd57856395a3fc04d8ae86538b0bff0d7e5
donMaleesha <lochanamt@gmail.com>
2024-07-08 21:10:48 +0530

    repetitive tasks with loops

---
+++ RepetitiveTasksWithLoops.js
@@ -0,0 +1,49 @@
+//Task 1
+
+var i;
+
+for (i = 1; i <= 5; i++) {
+
+    console.log(i);
+
+}
+
+console.log("Counting Completed!");
+
+//Task 2
+
+for (i = 5; i >0; i--) {
+
+    console.log(i);
+
+}
+
+console.log("Countdown Completed!");
+
+//Task 3
+
+var i = 1;
+
+while(i <= 5) {
+
+    console.log(i);
+
+    i++;
+
+}
+
+console.log("Counting Completed!");
+
+//Task 4
+
+var i = 5;
+
+while(i > 0) {
+
+    console.log(i);
+
+    i--;
+
+}
+
+console.log("Countdown Completed!");
