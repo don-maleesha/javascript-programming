7e6272991c6ce4f5eb851eecbdedd28f03f3781c
donMaleesha <lochanamt@gmail.com>
2024-07-10 10:37:31 +0530

    how to calling functions

---
+++ BuildingAndCallingFunctions.js
@@ -0,0 +1,21 @@
+function addnumbers() {
+
+    var num1 = 10;
+    var num2 = 20;
+    var sum = num1 + num2;
+    console.log(sum);
+
+}
+
+addnumbers(); //without arguments
+
+
+function addNumbers(num1, num2) {
+
+    var sum = num1 + num2;
+    console.log(sum);
+
+}
+
+addNumbers(10, 20); //with arguments
+addNumbers(30, 40); //with arguments
