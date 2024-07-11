674dd3614eb3b723f380cee040fa82a0a0f888f8
donMaleesha <lochanamt@gmail.com>
2024-07-11 20:23:20 +0530

    object literals and dot notation

---
+++ ObjectLiteralsAndDotNotation.js
@@ -0,0 +1,25 @@
+var assistantManager = {
+
+    // properties
+    rangeTilesPerTurn: 3,
+    socialSkills: 30,
+    streestSmarts: 30,
+    health: 40,
+    specialAbility: "young and energetic",
+    greeting: "Hello, how can I help you?",
+}
+
+console.log(assistantManager); // { rangeTilesPerTurn: 3, socialSkills: 30, streestSmarts: 30, health: 40, specialAbility: 'young and energetic', greeting: 'Hello, how can I help you?' }
+
+console.log(assistantManager.rangeTilesPerTurn); // 3
+
+var house = {}; // empty object
+
+house.bedrooms = 3;
+house.bathrooms = 2;
+house.kitchen = 1;
+house.livingRoom = 1;
+
+console.log(house); // { bedrooms: 3, bathrooms: 2, kitchen: 1, livingRoom: 1 }
+
+console.log(house.bedrooms); // 3
