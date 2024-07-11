459a7d9a2239ccf4c94b8f842fedf44016c1730f
donMaleesha <lochanamt@gmail.com>
2024-07-11 20:36:29 +0530

    object literals and dot notation

--- ObjectLiteralsAndDotNotation.js
+++ ObjectLiteralsAndDotNotation.js
@@ -1,3 +1,4 @@
+// ex01: Object literals and dot notation
 var assistantManager = {
 
     // properties
@@ -12,6 +13,8 @@ var assistantManager = {
 console.log(assistantManager); // { rangeTilesPerTurn: 3, socialSkills: 30, streestSmarts: 30, health: 40, specialAbility: 'young and energetic', greeting: 'Hello, how can I help you?' }
 
 console.log(assistantManager.rangeTilesPerTurn); // 3
+
+// ex02: Object literals and dot notation
 
 var house = {}; // empty object
 
@@ -31,3 +34,20 @@ car["model"] = "Camry";
 car["year"] = 2015;
 
 console.log(car); // { make: 'Toyota', model: 'Camry', year: 2015 }
+
+
+// ex03: Object literals and dot notation
+
+var arrOfKeys = ["make", "model", "year"]; // array of keys
+
+var drone = {
+    make: "DJI",
+    model: "Mavic Pro",
+    year: 2018
+}
+
+for (var i = 0; i < arrOfKeys.length; i++) {
+
+    console.log(drone[arrOfKeys[i]]); // DJI Mavic Pro 2018
+
+}
