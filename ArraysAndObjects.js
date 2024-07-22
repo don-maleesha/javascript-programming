1ea7fa79f29982c5c80b09ed726f9d39efed0443
donMaleesha <lochanamt@gmail.com>
2024-07-22 20:25:52 +0530

    arryas and objects

---
+++ ArraysAndObjects.js
@@ -0,0 +1,29 @@
+var clothes = []; // [] - array literal syntax
+
+clothes.push("shirt");
+clothes.push("pants");
+clothes.push("socks");
+clothes.push("scarf");
+clothes.push("hat");
+
+console.log(clothes);
+
+clothes.pop();
+
+console.log(clothes);
+
+clothes.push("coat");
+
+console.log(clothes);
+
+console.log(clothes[2]);
+
+
+var favCar = {}; // {} - object literal syntax
+
+favCar.color = "red";
+
+favCar.covertible = false;
+
+console.log(favCar);
+
