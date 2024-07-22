aa577bee8a926199b1d7d367d1caa57da341be89
donMaleesha <lochanamt@gmail.com>
2024-07-22 20:24:47 +0530

    string functions

---
+++ StringFunctions.js
@@ -0,0 +1,15 @@
+var greet = "Hello, ";
+
+var place = "World";
+
+console.log(greet.length); // 7 (6 characters + 1 space)
+
+console.log(greet.charAt(0)); // H
+
+
+console.log(greet.concat(place)); // Hello, World
+
+console.log(greet.indexOf("l")); // 2
+
+console.log(greet.toUpperCase); // HELLO,
+console.log(greet.toLowerCase); // hello,
