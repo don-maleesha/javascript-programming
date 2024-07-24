8cbe6c662898075e7bfd042be7018345b092a808
donMaleesha <lochanamt@gmail.com>
2024-07-24 20:18:26 +0530

    typeof function

---
+++ TypeOf.js
@@ -0,0 +1,15 @@
+var test1 = typeof("What is this?"); // string
+
+var test2 = typeof(1 < 2);
+
+var test3 = typeof([1, 2, 3]);
+
+var test4 = typeof({name: "John", age: 30});
+
+var test5 = typeof(function abc() {console.log("Hello World!")}); 
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+console.log(test5);
